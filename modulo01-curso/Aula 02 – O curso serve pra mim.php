e49b933a61a6636b71a655616ba357c917417385
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2023-03-21 22:54:56 -0300

    Aula 02 – O curso serve pra mim - concluído com sucesso!

---
+++ modulo01-curso/Aula 02 – O curso serve pra mim.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 02 – O curso serve pra mim?</title>
+</head>
+<body>
+    <h1>Aula 02 – O curso serve pra mim?</h1>
+    <h2>Este curso é para você se...</h2>
+    <p>"Já sei o básico de HTML, CSS e JS, quero continuar os estudos."</p>
+    <p>"Estou tendo essa matéria na faculdade e não entra na minha cabeça."</p>
+    <p>"Quero aprender PHP, mas não tenho um servidor disponível pra mim."</p>
+    <p>"Eu sei rodar código pronto, mas tenho dificuldades em criar meus scripts."</p>
+    <p>"Quero aprender o potencial de rodar scripts no servidor e criar funcionalidades."</p>
+    <p>"Já estudei PHP, mas com uma versão muito antiga."</p>
+    <h2>Este curso não é pra você se...</h2>
+    <p>"Não sou iniciante, já crio scripts básicos e sei fazer um CRUD simples."</p>
+    <p>"Estou procurando por códigos prontos e templates de sistemas feitos em PHP."</p>
+    <p>"Estou com pressa e preciso aprender PHP em poucas semanas."</p>
+    <p>"Quero só um guia rápido, sem enrolação e sem tanto detalhamento."</p>
+    <p>"Não tenho tempo para praticar e repetir os exercícios."</p>
+    <p>"Prefiro consultar a documentação oficial da linguagem PHP."</p>
+</body>
+</html>
