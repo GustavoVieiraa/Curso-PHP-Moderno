b88a283685c7425cfc4bc3cd3c32266fc770d1fc
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2023-03-21 21:24:47 -0300

    Aula 01 - Abertura - concluída com sucesso!

---
+++ modulo01-curso/Aula 01 – Abertura.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 01 – Abertura</title>
+</head>
+<body>
+    <h1>Aula 01 – Abertura</h1>
+    <h2>Nosso conteúdo do Módulo 01:</h2>
+    <ul>
+        <li>
+            Argumentação
+        </li>
+        <li>
+            Apresentação da Linguagem PHP
+        </li>
+        <li>
+            Funcionamento do PHP
+        </li>
+        <li>
+            Instalação das Ferramentas
+        </li>
+        <li>
+            Testes e Configurações
+        </li>
+        <li>
+            Estrutura de Scripts
+        </li>
+        <li>
+            Saída de Dados
+        </li>
+        <li>
+            Tipos e Variáveis
+        </li>
+        <li>
+            Entrada de Dados
+        </li>
+        <li>
+            Integração com Formulários
+        </li>
+        <li>
+            Operadores
+        </li>
+    </ul>
+</body>
+</html>
