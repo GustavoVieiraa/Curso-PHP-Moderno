49af78d3511dd35459488539517eb22dce2076df
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2023-03-25 19:37:09 -0300

    Aula 06 – Versões do PHP - concluída com sucesso!

---
+++ modulo01-curso/Aula 06 – Versões do PHP.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 06 – Versões do PHP</title>
+</head>
+<body>
+    <h1>Aula 06 – Versões do PHP</h1>
+    <h2>As versões do PHP</h2>
+    <ul> 1995 - PHP 1.0
+        <ul style="list-style-type: square"> 
+            <li>Personal Home Page.</li>
+            <li>Funcionalidades simples para um site básico.</li>
+            <li>Não tinha características de linguagem em si.</li>
+        </ul>
+    </ul>
+    <ul> 1997 - PHP 2.0
+        <ul style="list-style-type: square;">
+            <li>Oficialmente nomeado PHP/FI 2.0</li>
+            <li>Linguagem standalone</li>
+            <li>Recursos Limitados</li>
+        </ul>
+    </ul>
+    <ul> 1998 - PHP 3.0
+        <ul style="list-style-type: square;">
+            <li>Primeira versão colaborativa</li>
+            <li>Características mais relacionadas a uma linguagem</li>
+            <li>Exntesion API</li>
+        </ul>
+    </ul>
+    <ul> 2000 - PHP 4.0
+        <ul style="list-style-type: square;">
+            <li>Zend Engine 1.0</li>
+            <li>Melhoria de perfomance</li>
+            <li>Aumento da modularização</li>
+            <li>Uso de super globais $_GET, $_POST, $_SESSION, etc</li>
+        </ul>
+    </ul>
+    <ul> 2004 - PHP 5.0
+        <ul style="list-style-type: square;">
+            <li>Zend Engine 2.0</li>
+            <li>Primeira versão com Orientação a Objetos</li>
+            <li>PHP Data Objects (PDO)</li>
+            <li>Operador de exponenciação</li>
+            <li>Suporte a JSON</li>
+            <li>Namespaces, clousures, garbage collection, exceptions handling.</li>
+            <li>Novo depurador (phpdbg)</li>
+        </ul>
+    </ul>
+    <ul> 2005 - PHP 6.0
+        <ul style="list-style-type: square;">
+            <li>Aumento da segurança e performance</li>
+            <li>Suporte nativo a Unicode</li>
+            <li>Até existiram algumas versões</li>
+            <li><strong>Nunca recebeu uma release oficial</strong></li>
+        </ul>
+    </ul>
+    <ul> 2015 - PHP 7.0
+        <ul style="list-style-type: square;">
+            <li>Zend Engine 3.0</li>
+            <li>Performance até 9x maior</li>
+            <li>Escape de códigos Unicode</li>
+            <li>Operador null coalescing ??</li>
+            <li>Declaração escalar</li>
+            <li>Operador spaceship <=></li>
+            <li>Classes anônimas</li>
+            <li>Excessões mais modernas</li>
+            <li>Retorno de void</li>
+            <li>Libsodium</li>
+            <li>Foreign function interface</li>
+        </ul>
+    </ul>
+    <ul> 2020 - PHP 8.0
+        <ul style="list-style-type: square;">
+            <li>Zend Engine 4.0</li>
+            <li>Just-In-Time compilation</li>
+            <li>Locale-independent number convertions</li>
+            <li>Named arguments</li>
+            <li>Expressão match</li>
+            <li>Operador nullsafe</li>
+            <li>Enumerations</li>
+            <li>Readonly properties</li>
+            <li>Fibers</li>
+        </ul>
+    </ul>
+</body>
+</html>
