8e8f873d7aaeb136533ef33a12e0d4ce836d9433
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2023-03-25 17:11:53 -0300

    Aula 03 – Bibliografia recomenda - concluído com êxito!

---
+++ modulo01-curso/Aula 03 – Bibliografia recomenda.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>M01 Aula 03 – Bibliografia recomenda</title>
+</head>
+<body>
+    <h1>M01 Aula 03 – Bibliografia recomenda</h1>
+    <h2>Referências bibliográficas do curso:</h2>
+    <ul>
+        <li><a href="https://www.php.net/manual/pt_BR/" target="_blank">PHP Documentation</a></li>
+    </ul>
+    <p>o Professor Guanabara, indicou diversos livros para aprofundar em PHP, através de livros.</p>
+</body>
+</html>
