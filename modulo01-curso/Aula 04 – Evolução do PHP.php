e8fb6ec83ee32947c1b88c56b41def3c5a9c6ba6
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2023-03-25 18:00:28 -0300

    Aula 04 – Evolução do PHP - concluído com êxito!

---
+++ modulo01-curso/Aula 04 – Evolução do PHP.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body
+        {
+            text-align: center;
+        }
+    </style>
+    <title>Aula 04 – Evolução do PHP</title>
+</head>
+<body>
+    <h1>Aula 04 – Evolução do PHP</h1>
+    <h2>O surgimento</h2>
+    <p>o PHP surgiu em 1994 com o Rasmus Lerdorf. Ele fez grandes contribuições para o Apache (Software utilizado para transformar maquinas em servidores), e projetos do MySQL (Gerenciador de Banco de Dados). Ele foi um dos desenvolvedores que fez a implementação do limit dentro do MySQL. Em 1994, ele teve um problema, ele tinha um curriculo online em seu site, e ele queria contar quantas pessoas estavam visitando seu curriculo. Então, ele desenvolveu uma CGI (Common Gateway Interface), utilizando a linguagem C para desenvolver essa funcionalidade, e o nome que ele deu para essa ferramenta foi Personal Home Page, dando origem a cigra PHP.</p>
+    <h2>Primeiras Atualizações</h2>
+    <p>E foi em, 1995 que ele empacotou todas as funções do PHP e colocou em uma versão, no PHP 1.0,conhecido na época como PHP Tools.</p>
+    <h2>Segunda Versão</h2>
+    <p>Um ano depois, em 1996, ele lançou a versão PHP 2.0, que era o PHP/FI.</p>
+    <p>Como curiosidade, já em 96, quase 15 mil sites usavam o PHP/FI em suas linhas.</p>
+    <p>E chegou em um ponto, que ele não conseguiu mais mantem toda aquela estrutura sozinho, e o PHP passou a ser um software Livre (OPEN SOURCE).</p>
+    <h2>Uma nova vida ao PHP</h2>
+    <p>E aqui, em 1997, em Israel Institute of Technology, houve uma das maiores contribuições da linguagem, onde dois estudantes do instituto, Andi Gutmans e Zeev Suraski, estavam desenvolvendo um software de uma loja, e tiveram contato com o projeto do PHP. Eles em entrevista disseram que "o PHP tinha potencial, mas não atendia todas as suas demandas", então, eles foram conversar com o Rasmus e ofereceram ajuda por se tratar de um software aberto. De 1997 a 1998, eles rescreveram o código PHP, e foi à partir daí que o PHP começou a tomar cara de linguagem de programação mesmo.</p>
+    <p>Então, foi em 1998 que eles lançaram o PHP3, e tiveram que alterar o nome de <strong>Personal Home Page</strong> para <strong>PHP: Hypertext Preprocessor</strong></p>
+    <h2>Logotipo e Mascote</h2>
+    <p>E ainda em 1998, dois caras foram muito importantes para o PHP, Colin Viebrock e Vincent Pontier. <strong>Colin</strong>, foi o responsável pelo desenho do logotipo do PHP, e o <strong>Vincent</strong>, foi o cara que desenho o mascote da linguagem PHP (Elefantinho).</p>
+    <h2>A nova criadora do PHP</h2>
+    <p>O Zeev e o Andi, estavam tão colaborativos com o projeto do PHP, que eles fundaram uma empresa especializada no desenvolvimento da linguagem. Pra isso, eles criaram a <strong>Zead</strong></p>
+    <h2>O PHP cresceu...</h2>
+    <p>E a partir daí, o PHP cresceu MUITO, desde a criação da Zead Engine, vieram diversas versões, 4.0, 5.0, 6.0 <- ele se quer existiu..., 7.0, 8.0.</p>
+</body>
+</html>
