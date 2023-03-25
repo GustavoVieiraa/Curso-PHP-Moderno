0ec8e26f38e2f614315ea8bbe53b157f9e2c3efe
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2023-03-25 18:53:17 -0300

    Aula 05 – ElePHPant - concluído com sucesso!

---
+++ modulo01-curso/Aula 05 – ElePHPant.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 05 – ElePHPant</title>
+</head>
+<body>
+    <h1>Aula 05 – ElePHPant</h1>
+    <h2>O mascote do PHP</h2>
+    <p>Nessa aula, o Professor Guanabara, explicou mais a curiosidade de como o PHP, é representado como um elefante, o mascote do PHP.</p>
+    <p><strong>ElePHPant</strong></p>
+</body>
+</html>
